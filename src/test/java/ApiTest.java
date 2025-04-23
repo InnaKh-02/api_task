b2ac9033a759d5853379ba9b8c90737be1728c28
Inna Kononenko <128909459+InnaKh-02@users.noreply.github.com>
2025-04-23 13:20:33 +0300

    Update ApiTest.java

--- src/test/java/ApiTest.java
+++ src/test/java/ApiTest.java
@@ -1,34 +1,84 @@
 import org.junit.jupiter.api.*;
-import service.UserService;
-import static org.junit.jupiter.api.Assertions.*;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
 
 public class ApiTest {
-    private UserService userService;
 
-    @BeforeEach
-    public void setup() {
-        userService = new UserService();
-    }
+    private final String endpointUsers = "https://jsonplaceholder.typicode.com/users";
+    private final String endpointPosts = "https://jsonplaceholder.typicode.com/posts";
 
     @Test
     @DisplayName("Verify that the response status code is 200 OK")
     public void testStatusCodeIs200() {
-        int statusCode = userService.getStatusCode();
-        assertEquals(200, statusCode, "Status code should be 200");
+        given().
+            when().
+                get(endpointUsers).
+        then().
+            assertThat().
+                statusCode(200);
     }
 
     @Test
     @DisplayName("Verify that the Content-Type header is 'application/json; charset=utf-8'")
     public void testContentTypeHeader() {
-        String contentType = userService.getContentType();
-        assertNotNull(contentType, "Content-Type header should exist");
-        assertEquals("application/json; charset=utf-8", contentType, "Content-Type value should match expected");
+        given().
+            when().
+                get(endpointUsers).
+        then().
+            assertThat().
+                header("Content-Type", notNullValue()).
+                header("Content-Type", equalTo("application/json; charset=utf-8"));
     }
 
     @Test
     @DisplayName("Verify that the response body contains 10 users")
     public void testResponseBodyHas10Users() {
-        int userCount = userService.getUsersCount();
-        assertEquals(10, userCount, "User count should be 10");
+        given().
+            when().
+                get(endpointUsers).
+        then().
+            assertThat().
+                body("size()", equalTo(10));
+    }
+
+    @Test
+    @DisplayName("Verify that a post is deleted successfully")
+    public void testAbsentPost() {
+        int postId = 1;
+        given()
+        .when()
+            .delete(endpointPosts + "/" + postId)
+        .then().
+            assertThat()
+                .statusCode(200);
+    }
+
+
+    @Test
+    @DisplayName("Verify that a post is updated successfully")
+    public void testUpdatePost() {
+        int postId = 20;
+        String body = """
+            {
+            "userId": 3,
+            "id": 20,
+            "title": "doloribus ad provident suscipit at",
+            "body": "qui consequuntur ducimus possimus quisquam amet similique\\nsuscipit porro ipsam amet\\neos veritatis officiis exercitationem vel fugit aut necessitatibus totam\\nomnis rerum consequatur expedita quidem cumque explicabo"
+            }
+            """;
+        given()
+            .header("Content-Type", "application/json; charset=UTF-8")
+            .body(body)
+        .when()
+            .put(endpointPosts + "/" + postId)
+        .then()
+            .assertThat()
+                .statusCode(200)
+                .body("userId", equalTo(3))
+                .body("id", equalTo(20))
+                .body("title", equalTo("doloribus ad provident suscipit at"))
+                .body("body", equalTo("qui consequuntur ducimus possimus quisquam amet similique\nsuscipit porro ipsam amet\neos veritatis officiis exercitationem vel fugit aut necessitatibus totam\nomnis rerum consequatur expedita quidem cumque explicabo"));
     }
 }
